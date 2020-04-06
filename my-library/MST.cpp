66204d239c7c1a07c665d8549a929ae04a9bfc3e
Denis Akilov <54374832+seiko-iwasawa@users.noreply.github.com>
2020-04-06 18:20:29 +0300

    Add files via upload

---
+++ my-library/MST.cpp
@@ -0,0 +1,48 @@
+// unchecked
+
+#include <iostream>
+#include <algorithm>
+#include <numeric> // iota()
+#include <cassert>
+
+using namespace std;
+
+const int N = 1e5;
+
+int par[N], sz[N];
+
+void build() {
+	iota(par, par + N, 0);
+	fill(sz, sz + N, 1);
+}
+
+int find(int x) { return x == par[x] ? x : par[x] = find(par[x]); }
+
+bool join(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x != y) {
+		if (sz[x] > sz[y]) {
+			swap(x, y);
+		}
+		par[x] = y;
+		sz[y] += sz[x];
+	}
+	return x != y;
+}
+
+const int M = 1e6;
+
+struct Edge { int v, u, w; } edges[M];
+
+void build_mst() {
+	sort(edges, edges + M, [](Edge q, Edge w) { return q.w < w.w; });
+	int cnt = 0;
+	build();
+	for (Edge *e = edges; e != edges + M; ++e) {
+		if (join(e->v, e->u)) {
+			swap(*e, *(edges + cnt++));
+		}
+	}
+	assert(cnt == N - 1);
+}
