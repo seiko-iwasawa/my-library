95bfef4ed0ee8063855711a1f55b8057b1f504f3
Denis Akilov <54374832+seiko-iwasawa@users.noreply.github.com>
2020-04-06 19:35:48 +0300

    Add files via upload

--- my-library/Segment_Tree.cpp
+++ my-library/Segment_Tree.cpp
@@ -24,6 +24,7 @@ void build(int pos, int l, int r) {
 }
 
 void upd(int pos, int l, int r, int i, int x) {
+	// Complexity: O(logN)
 	if (i < l || r <= i) {
 		return;
 	}
@@ -38,6 +39,7 @@ void upd(int pos, int l, int r, int i, int x) {
 }
 
 int get(int pos, int l, int r, int ql, int qr) {
+	// Complexity: O(logN)
 	if (qr <= l || r <= ql) {
 		return -INF;
 	}
