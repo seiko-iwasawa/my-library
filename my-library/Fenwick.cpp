44a1dfba0ae08b412776d81185242c62f7fb41d0
Denis Akilov <54374832+seiko-iwasawa@users.noreply.github.com>
2020-04-06 19:32:04 +0300

    Add files via upload

--- my-library/Fenwick.cpp
+++ my-library/Fenwick.cpp
@@ -12,12 +12,14 @@ int n;
 ll fenw[N];
 
 void add(int i, int x) {
+	// Compexity: O(logN)
 	for (; i < N; i = (i | i + 1)) {
 		fenw[i] += x;
 	}
 }
 
 ll get(int i) {
+	// Complexity: O(logN)
 	ll res = 0;
 	for (; i >= 0; i = (i & i + 1) - 1) {
 		res += fenw[i];
@@ -26,5 +28,6 @@ ll get(int i) {
 }
 
 ll get(int i, int j) {
+	// Comlpexity: O(logN)
 	return get(j) - get(i - 1);
 }
