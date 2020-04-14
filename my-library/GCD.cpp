25a363040580856debbf71e305786a8ac1418068
Denis Akilov <54374832+seiko-iwasawa@users.noreply.github.com>
2020-04-14 21:34:13 +0300

    Add files via upload

---
+++ my-library/GCD.cpp
@@ -0,0 +1,26 @@
+// unchecked
+
+#include <iostream>
+
+using namespace std;
+
+int gcd1(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+	else {
+		return gcd1(b, a % b);
+	}
+}
+
+int gcd2(int a, int b) {
+	while (b) {
+		a %= b;
+		swap(a, b);
+	}
+	return a;
+}
+
+int gcd3(int a, int b) {
+	return b ? gcd3(b, a % b) : a;
+}
