8594493f801705ff60824766f223b937407593e5
Denis Akilov <54374832+seiko-iwasawa@users.noreply.github.com>
2020-04-19 00:42:50 +0300

    Update Fenwick.cpp

--- my-library/Fenwick.cpp
+++ my-library/Fenwick.cpp
@@ -1,15 +1,11 @@
-// unchecked
-
 #include <iostream>
 
 using namespace std;
 
-typedef long long ll;
-
 const int N = 1e5;
 
 int n;
-ll fenw[N];
+int fenw[N];
 
 void add(int i, int x) {
 	// Compexity: O(logN)
@@ -18,16 +14,16 @@ void add(int i, int x) {
 	}
 }
 
-ll get(int i) {
+int get(int i) {
 	// Complexity: O(logN)
-	ll res = 0;
+	int res = 0;
 	for (; i >= 0; i = (i & i + 1) - 1) {
 		res += fenw[i];
 	}
 	return res;
 }
 
-ll get(int i, int j) {
+int get(int i, int j) {
 	// Comlpexity: O(logN)
 	return get(j) - get(i - 1);
 }
