bae838de162d7a940d488104444de01ab3ca157f
Denis Akilov <54374832+seiko-iwasawa@users.noreply.github.com>
2020-06-02 20:16:18 +0300

    Add files via upload

---
+++ my-library/Z_func.cpp
@@ -0,0 +1,32 @@
+// unchecked
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1e5;
+
+string s;
+int zf[N];
+
+void build_zf() {
+	int n = s.size();
+	fill(zf, zf + n, 0);
+	for (int i = 1, l = 0, r = -1; i < n; ++i) {
+		if (i < r) {
+			zf[i] = min(r - i, zf[i - l]);
+		}
+		while (s[zf[i]] == s[i + zf[i]]) {
+			++zf[i];
+		}
+		if (r < i + zf[i]) {
+			l = i, r = i + zf[i];
+		}
+	}
+}
+
+int main() {
+	s = "abcaabcab";
+	build_zf();
+}
