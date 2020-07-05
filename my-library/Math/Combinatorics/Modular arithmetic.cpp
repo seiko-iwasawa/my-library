5be1baff5fb55d13b8525973405d96e54b9d9aea
Denis Akilov <54374832+seiko-iwasawa@users.noreply.github.com>
2020-07-05 23:08:12 +0300

    Update and rename Modular_arithmetic.cpp to Modular arithmetic.cpp

--- my-library/Math/Combinatorics/Modular arithmetic.cpp
+++ my-library/Math/Combinatorics/Modular arithmetic.cpp
@@ -1,5 +1,3 @@
-// unchecked
-
 #include <iostream>
 #include <vector>
 
@@ -9,8 +7,8 @@ const int MOD = 1e9 + 7;
 int sum(int x, int y) { return x + y < MOD ? x + y : x + y - MOD; }
 int dif(int x, int y) { return x - y < 0 ? x - y + MOD : x - y; }
 int mul(int x, int y) { return x * 1LL * y % MOD; }
-void add(int &x, int y) { x = sum(x, y); }
-void sub(int &x, int y) { x = dif(x, y); }
+int add(int &x, int y) { return x = sum(x, y); }
+int sub(int &x, int y) { return x = dif(x, y); }
 int qp(int x, int k) {
 	// Complexity: O(log(k))
 	int res = 1;
@@ -24,11 +22,11 @@ int qp(int x, int k) {
 }
 int inv(int x) { return qp(x, MOD - 2); }
 int dv(int x, int y) { return mul(x, inv(y)); }
-vector<int> _fact = { 1 };
 int fact(int x) {
-	while (_fact.size() <= x) {
-		_fact.push_back(mul(_fact.back(), _fact.size()));
+	static vector<int> pf = { 1 };
+	while (pf.size() <= x) {
+		pf.push_back(mul(pf.back(), pf.size()));
 	}
-	return _fact[x];
+	return pf[x];
 }
 int c(int n, int k) { return n < k ? 0 : dv(fact(n), mul(fact(k), fact(n - k))); }
